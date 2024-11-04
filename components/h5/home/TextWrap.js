a11d8a6af449b2bf9f258ef00e237831d4cebd9c
token.ens <token.ens@gmail.com>
2024-11-04 14:21:16 +0800

    Fix

--- components/h5/home/TextWrap.js
+++ components/h5/home/TextWrap.js
@@ -3,10 +3,10 @@ import React from "react";
 const TextWrap = ({ children }) => {
   return (
     <>
-    <div className="text-[3.2vw] sm:text-[22px] text-[rgba(255,255,255,0.6)] tracking-wide sm:tracking-wider leading-[4.8vw] sm:leading-6 block sm:hidden font-fnormal">
+    <div className="text-[3.2vw] sm:text-[22px] text-[rgba(255,255,255,0.6)] tracking-wide sm:tracking-wider leading-[4.8vw] sm:leading-6 block sm:hidden font-exlt">
       { children }
     </div>
-    <div className="text-[3.2vw] text-justify sm:text-[20px] text-[rgba(255,255,255,0.6)] leading-[4.8vw] sm:leading-[150%] tracking-wide sm:tracking-wider sm:text-left hidden sm:block font-fnormal">
+    <div className="text-[3.2vw] text-justify sm:text-[20px] text-[rgba(255,255,255,0.6)] leading-[4.8vw] sm:leading-[150%] tracking-wide sm:tracking-wider sm:text-left hidden sm:block font-exlt">
       { children }
     </div>
     </>
